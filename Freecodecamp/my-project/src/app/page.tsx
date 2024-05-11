a669e1c2ec64a2881cd44a28d0448a9cd1c9da4e
itzaRinna <dutchismahdick@gmail.com>
2024-05-11 13:33:05 +0100

    customization features

--- Freecodecamp/my-project/src/app/page.tsx
+++ Freecodecamp/my-project/src/app/page.tsx
@@ -3,12 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="border-4 border-green-600">
-        Hello world!
-      </h1>
-      <h1 className="text-vuejs-100">
-        Hello world!
-      </h1>
+      <p className="">Hello long sentence</p>
+      <p>Another long sentence</p>
     </div>
   );
 }
--- Freecodecamp/my-project/tailwind.config.js
+++ Freecodecamp/my-project/tailwind.config.js
@@ -9,11 +9,35 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
+      spacing: {
+        '1': '80px',
+        '2': '12px',
+        '3': '16px',
+        '4': '24px',
+        '5': '32px',
+        '6': '48px',
+      },
       colors: {
         vuejs: {
           100: "#49e659",
-          932: "#434252"
+          900: "#434252" // Changed from 932 to 900
         }
       }
     }
